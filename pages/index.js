97827b9f7a4dc0038334708444e66605302e742d
iscole <isaiah.coleman@sas.com>
2019-08-03 19:14:06 -0400

    Adding the nextjs less plugin and some styling

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import {properties} from './src/properties.js';
 import { OauthSender } from 'react-oauth-flow';
+import "../pages/styles/main.less"
 
-import './styles/main.css';
 
 class SubmitForm extends React.Component {
 
@@ -43,22 +43,22 @@ class SubmitForm extends React.Component {
 		//Pass login provider in state since we're going to do the token request on the 'server'
 		const state = {idp: 'ms'};
 		return (
-			<div>
-				<label htmlFor="clientID"> Client ID: </label>
-				<input name="clientID" value={this.state.clientID} onChange={this.handleInputChange}/>
-				<label htmlFor="clientSecret"> Client Secret: </label>
-				<input name="clientSecret" value={this.state.clientSecret} onChange={this.handleInputChange}/>
-				<label htmlFor="authorizationURL"> Authorization URL: </label>
-				<input name="authorizationURL"  value={this.state.authorizationURL} onChange={this.handleInputChange}/>
-				<label htmlFor="scope"> Scope: </label>
-				<input name="scope"  value={this.state.scope} onChange={this.handleInputChange}/>
+			<div className='submit-form'>
+				<div className='form-inputs'>
+					<label htmlFor="clientID"> Client ID </label>
+					<input name="clientID" value={this.state.clientID} onChange={this.handleInputChange}/>
+					<label htmlFor="authorizationURL"> Authorization URL </label>
+					<input name="authorizationURL"  value={this.state.authorizationURL} onChange={this.handleInputChange}/>
+					<label htmlFor="scope"> Scope </label>
+					<input name="scope"  value={this.state.scope} onChange={this.handleInputChange}/>
+				</div>
 				<OauthSender
         			authorizeUrl={this.state.authorizationURL}
         			clientId={this.state.clientID}
         			redirectUri="http://localhost:3000/oauthRedirect"
         			state={state}
         			args={sendArgs}
-        			render={({ url }) => <button onClick={this.initHandshake} data-login-url={url} disabled={this.state.isDisabled}>Login</button>}
+        			render={({ url }) => <button onClick={this.initHandshake} data-login-url={url} disabled={this.state.isDisabled}>LOGIN</button>}
       			/>
 			</div>
 		);
@@ -69,7 +69,6 @@ class App extends React.Component {
 	render () {
 		return (
 			<div>
-				<p>Hello Next.js</p>
 				<SubmitForm />
 			</div>
 		);
